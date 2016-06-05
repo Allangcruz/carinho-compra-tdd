4ee226c4fa7f6693608f447d8fc71870696017f5
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2016-06-05 15:39:57 -0300

    Demais teste

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -2,6 +2,41 @@
 
 namespace AllanGCruz\Carrinho\Entities;
 
+use Doctrine\Instantiator\Exception\InvalidArgumentException;
+
 class Product implements ProductInterface {
+	protected $name;
+	protected $description;
+	protected $price;
+
 	
+	public function setName($name) {
+		$this->name = $name;
+	}	
+
+	public function getName() {
+		return $this->name;
+	}
+	
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+
+	public function getDescription() {
+		return $this->description;
+	}
+	
+	public function setPrice($price) {
+
+		if(!is_numeric($price)) {
+			throw new InvalidArgumentException;
+		}
+
+		$this->price = $price;
+	}
+
+	public function getPrice() {
+		return $this->price;
+	}
+
 }
--- tests/Entities/ProductTest.php
+++ tests/Entities/ProductTest.php
@@ -11,4 +11,33 @@ class ProductTest extends \PHPUnit_Framework_TestCase {
 		$product = new Product();
 		$this->assertInstanceOf(ProductInterface::class, $product);
 	}
+
+	public function testProductNameValue() {
+		$product = new Product();
+		$product->setName("Product 1");
+
+		$this->assertEquals("Product 1", $product->getName());
+	}
+
+	public function testDescriptionValue() {
+		$product = new Product();
+		$product->setDescription("Description 1");
+
+		$this->assertEquals("Description 1", $product->getDescription());
+	}
+
+	public function testPriceValue() {
+		$product = new Product();
+		$product->setPrice(10);
+
+		$this->assertEquals(10, $product->getPrice());
+	}
+
+	/**
+	* @expectedException InvalidArgumentException
+	*/
+	public function testPriceValueWhenANotNumericGiven() {
+		$product = new Product();
+		$product->setPrice("abc");
+	}
 }
